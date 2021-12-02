daeba7fb18b3d36d2b264bc16ffa708516e674c3
Dimitrios Gkegkas <dimitrisgegas01@gmail.com>
2021-12-02 13:29:25 +0200

    Nice example

--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,23 @@ import { Wit } from "./model/Wit";
 import { WitToken } from "./secret";
 
 (() => {
-    const wit = new Wit(WitToken);
-    // populate(
-    //     "Μπορώ να συνδυάσω παιχνίδια στα Virtuals;",
-    //     "Μπορείς να παίξεις παρολί συνδυάζοντας παιχνίδια από το ίδιο άθλημα ή και από διαφορετικά αθλήματα.",
-    //     "virtual_sports_combine_info"
-    // );
-    wit.Meaning("πως υπολογισμος cash out");
-    wit.Train([{
-            text: "Content",
-            intent: "key",
-            entities: [],
-            traits: [],
-    }])
+    populate(
+        "Μπορώ να συνδυάσω παιχνίδια στα Virtuals;",
+        "Μπορείς να παίξεις παρολί συνδυάζοντας παιχνίδια από το ίδιο άθλημα ή και από διαφορετικά αθλήματα.",
+        "virtual_sports_combine_info"
+    );
 })();
 
-// export async function populate(qst: string, ans: string, key: string) {
-//     const augmenter = new Augmenter(cwd() + "/src/datasets");
-//     augmenter.push([
-//         {
-//             qst,
-//             ans,
-//             key,
-//         },
-//     ]);
-//     await augmenter.byTranslation();
-//     IntentAndTrain(key, augmenter.getReadyForTrain(key));
-// }
+export async function populate(qst: string, ans: string, key: string) {
+    const wit = new Wit(WitToken);
+    const augmenter = new Augmenter(cwd() + "/src/datasets");
+    augmenter.push([
+        {
+            qst,
+            ans,
+            key,
+        },
+    ]);
+    await augmenter.byTranslation();
+    wit.Train(augmenter.getReadyForTrain(key));
+}
